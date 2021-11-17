84fb92c91ca0cc3eabe32b6f8b56b40e988f7d0d
Benjamin Hamelin <b.hamelin@student.isartdigital.com>
2021-11-17 18:16:30 +0100

    windows debug part commented

--- HypercasualGameTest/Assets/Scripts/PlayerController.cs
+++ HypercasualGameTest/Assets/Scripts/PlayerController.cs
@@ -30,31 +30,15 @@ public class PlayerController : MonoBehaviour
 
         /////////////////////////////////////////////////////////// Android / IOS
 
-        //Vector3 pos = transform.position;
-
-        //if (Input.touchCount > 0)
-        //{
-        //    Touch touch = Input.GetTouch(0);
-
-        //    Vector2 delta = touch.deltaPosition;
-
-        //    print(touch.deltaPosition);
-
-        //    pos += new Vector3(delta.x, 0f, 0f) * xSpeed * Time.fixedDeltaTime;
-        //    pos.x = Mathf.Clamp(pos.x, -maxOffset, maxOffset);
-        //}
-
-        //pos.z += zSpeed * Time.deltaTime;
-        //transform.position = pos;
-
-
-        /////////////////////////////////////////////////////////// WINDOWS DEBUG
-        
         Vector3 pos = transform.position;
 
-        if (Input.GetMouseButton(0))
+        if (Input.touchCount > 0)
         {
-            Vector2 delta = new Vector2(Input.mousePosition.x, Input.mousePosition.y) - lastMousePos;
+            Touch touch = Input.GetTouch(0);
+
+            Vector2 delta = touch.deltaPosition;
+
+            print(touch.deltaPosition);
 
             pos += new Vector3(delta.x, 0f, 0f) * xSpeed * Time.fixedDeltaTime;
             pos.x = Mathf.Clamp(pos.x, -maxOffset, maxOffset);
@@ -63,7 +47,23 @@ public class PlayerController : MonoBehaviour
         pos.z += zSpeed * Time.deltaTime;
         transform.position = pos;
 
-        lastMousePos = Input.mousePosition;
+
+        /////////////////////////////////////////////////////////// WINDOWS DEBUG
+
+        //Vector3 pos = transform.position;
+
+        //if (Input.GetMouseButton(0))
+        //{
+        //    Vector2 delta = new Vector2(Input.mousePosition.x, Input.mousePosition.y) - lastMousePos;
+
+        //    pos += new Vector3(delta.x, 0f, 0f) * xSpeed * Time.fixedDeltaTime;
+        //    pos.x = Mathf.Clamp(pos.x, -maxOffset, maxOffset);
+        //}
+
+        //pos.z += zSpeed * Time.deltaTime;
+        //transform.position = pos;
+
+        //lastMousePos = Input.mousePosition;
     }
 
 
